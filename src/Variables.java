dd85a960f418dabe2ab19fd003936708a2494568
xa4ax2000 <andrew.hyun@alumni.stonybrook.edu>
2016-07-25 06:50:31 -0400

    Add files via upload

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+
+public class Variables {
+	public static void main(String args[]){
+		double tuna;
+		tuna = 5.22;
+		double salmon;
+		salmon = 9.78;
+		System.out.println("a: " + tuna + " b: " + salmon + " = \n");
+	}
+}
